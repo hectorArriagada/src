ba54ab3422f3ce81cac0fac559ae69abb471a760
hectorArriagada <hect.arriagada@duocuc.cl>
2024-09-02 08:28:18 -0400

    actualizacion entrega 1.2 skeletonapp
    actualizacion login, navigation extras, se agrega la pagina home con los datos y se crea la page registro

--- app/home/home.page.ts
+++ app/home/home.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  alertButtons = ['Salir'];
 
-}
+  usuario = '';
+  user = {
+    nombre: '',
+    apellido: '',
+
+  };
+
+  constructor(private router: Router) {
+    const navegacion = this.router.getCurrentNavigation();
+    const state = navegacion?.extras.state as {
+      usuario: '';
+      clave: '';
+    };
+    this.usuario = state.usuario;
+  }
+      
+  ngOnInit() {
+  }
+  
+  }
--- app/login/login.page.ts
+++ app/login/login.page.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-login',  
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  /* objeto JSON para el usuario */
+  user = {
+    usuario: '',
+    clave: '',
+  };
+
+  /* mensaje de respuesta */
+  mensaje = '';
+
+  /* estado de carga */
+  spinner = false;
+
+
+
+  constructor(private router: Router, private animationController: AnimationController) {}
+
+  cambiarSpinner() {
+    this.spinner = !this.spinner;
+  }
+
+  validar() {
+    if (this.user.usuario.length != 0) {
+      if (this.user.clave.length != 0) {
+        //funciona
+        this.mensaje = 'Conexión Exitosa!';
+        let navigationExtras: NavigationExtras = {
+          state: {
+            usuario: this.user.usuario,
+            clave: this.user.clave,
+          },
+        };
+
+        this.cambiarSpinner();
+        setTimeout(() => {
+          this.router.navigate(['/home'], navigationExtras);
+          this.cambiarSpinner();
+          this.mensaje = "";
+        }, 1);
+
+      } else {
+        this.mensaje = 'Contraseña vacía';
+      }
+      
+    } else {
+      this.mensaje = 'Usuario vacío';
+    }
+  }
+
 
   ngOnInit() {
   }
 
+  
 }
